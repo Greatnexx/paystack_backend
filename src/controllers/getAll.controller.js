6e66d6a9d63274fa5255ddfa4a6f8b64ea358b1f
Greatnexx <godwindanielgodwin@gmail.com>
2025-01-23 05:00:25 +0100

    added new files

--- src/controllers/getAll.controller.js
+++ src/controllers/getAll.controller.js
@@ -34,3 +34,4 @@ export const getAllPayments = async (req, res) => {
     });
   }
 };
+
--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 import app from "./app.js";
 import connectDb from "./config/db.js";
 
+// Connect to MongoDB
 connectDb();
 
 if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 3000;
+   const PORT = process.env.PORT || 3000;
   
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-}
 
+}
