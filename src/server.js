d8730d23d447c779602e80a943345dadba085f7d
Greatnexx <godwindanielgodwin@gmail.com>
2025-01-23 05:08:44 +0100

    exported file

--- src/server.js
+++ src/server.js
@@ -4,11 +4,13 @@ import connectDb from "./config/db.js";
 // Connect to MongoDB
 connectDb();
 
-if (process.env.NODE_ENV !== 'production') {
-   const PORT = process.env.PORT || 3000;
-  
+// Listen only in a local environment
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-
 }
+
+// Export the app for Vercel (required for serverless deployment)
+export default app;
